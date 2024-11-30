6c93d540bceabc0cde58c9a704e52c4ad353705a
Thiago G <thiagogale13@hotmai.com>
2024-11-30 18:21:53 -0300

    Agregado admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    list_display = ('correo_electronico', 'nombre_de_usuario', 'edad', 'es_superusuario', 'esta_activo')
+    list_filter = ('es_superusuario',)
+    fieldsets = (
+        (None, {'fields': ('correo_electronico', 'password')}),
+        ('Información Personal', {'fields': ('nombre_de_usuario', 'edad')}),
+        ('Permisos', {'fields': ('es_superusuario', 'es_personal', 'esta_activo')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('correo_electronico', 'nombre_de_usuario', 'edad', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('correo_electronico',)
+    ordering = ('correo_electronico',)
+    filter_horizontal = ()
+
+admin.site.register(CustomUser, UserAdmin)
+
